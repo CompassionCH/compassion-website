570dc4f48ac4eb9cdd68830e24b2c960361e0502
Clément <clement.charmillot@gmail.com>
2024-08-07 09:24:05 +0200

    chore: format

--- website_event_compassion/models/res_lang.py
+++ website_event_compassion/models/res_lang.py
@@ -10,13 +10,10 @@ from odoo import models
 
 
 class ResLang(models.Model):
-    _inherit = 'res.lang'
+    _inherit = "res.lang"
 
     def format(self, percent, value, grouping=False, monetary=False):
         res = super().format(percent, value, grouping, monetary)
-        if self.code == 'fr_CH':
+        if self.code == "fr_CH":
             res = res.replace("'", "&nbsp;")
         return res
-
-
-
