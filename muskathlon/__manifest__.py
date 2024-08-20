17ea8bb28d2aef31a237e643234843e453e61146
Clément <100228798+clementcharmillot@users.noreply.github.com>
2024-08-20 13:37:04 +0200

    fix: fix invalid version string

--- muskathlon/__manifest__.py
+++ muskathlon/__manifest__.py
@@ -29,7 +29,7 @@
 # pylint: disable=C8101
 {
     "name": "Muskathlon",
-    "version": "14.0.1.2.01",
+    "version": "14.0.1.2.1",
     "category": "Reports",
     "author": "Compassion CH",
     "license": "AGPL-3",
