ae133f1931798f1de96450ad27ab97bd88bd7209
Niels Lachat <nlachat@compassion.ch>
2024-11-14 11:34:49 +0100

    Add warning if user tries to send/preview a letter with an empty text

--- my_compassion/static/src/js/write_a_letter.js
+++ my_compassion/static/src/js/write_a_letter.js
@@ -261,6 +261,12 @@ async function createLetter(mode = "preview") {
   startStopLoading(mode);
   const params = new URLSearchParams(window.location.search);
 
+  if (letter_content.value.length === 0){
+    displayAlert("letter_text_empty_warning");
+    startStopLoading(mode);
+    return;
+  }
+
   const json_data = {
     body: letter_content.value,
     template_id: params.get("template_id"),
