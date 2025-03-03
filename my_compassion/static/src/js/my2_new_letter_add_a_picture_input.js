7495527621e1e626187359ffdd9be8dab0952d99
NoeBerdoz <berdoznoe@gmail.com>
2025-03-03 15:40:18 +0100

    [MC2] - Refactor JS files naming

---
+++ my_compassion/static/src/js/my2_new_letter_add_a_picture_input.js
@@ -0,0 +1,8 @@
+/**
+ * Handles the uploaded attachments from the user when filling the new letter form
+ *
+ * Is used in /templates/pages/my2_new_letter.xml
+ */
+document.addEventListener('DOMContentLoaded', function () {
+
+});
