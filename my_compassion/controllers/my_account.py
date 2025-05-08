9f79a3dc3be3c7f1b02e46fb6b4e17aa7b29064d
Nath-S07 <nathanaelsingh076@gmail.com>
2025-05-08 15:46:13 +0200

    [T2393] Redirection from MyCompassion to make a donation language fix
    Signed-off-by: Nath-S07 <nathanaelsingh076@gmail.com>

--- my_compassion/controllers/my_account.py
+++ my_compassion/controllers/my_account.py
@@ -422,7 +422,7 @@ class MyAccountController(CustomerPortal):
         }
         # Construct query string with user profile info
         query_string = urlencode(partner_profile_info)
-        lang = request.env.context.get('lang') or partner.lang
+        lang = request.env.context.get('lang')
         lang_prefix = f"{lang.split('_')[0]}" if lang else None
         url_child_gift = (
             (f"https://{wordpress.host}/{lang_prefix}{wordpress.child_gift_url}?{query_string}")
