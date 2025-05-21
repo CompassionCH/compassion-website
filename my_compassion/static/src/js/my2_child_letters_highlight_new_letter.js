be167b713e4cb66ae27853d76f15606420e0ea35
MarcoPontarolo <ponta.pontarolo@hotmail.com>
2025-05-21 09:04:36 +0200

    T2310 create read to read letter logic - no refresh needed

--- my_compassion/static/src/js/my2_child_letters_highlight_new_letter.js
+++ my_compassion/static/src/js/my2_child_letters_highlight_new_letter.js
@@ -6,14 +6,31 @@
  *
  * Is used in /templates/pages/my2_child_letters.xml
  */
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const newLetterGeneratorId = urlParams.get("new_letter_generator_id");
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const newLetterGeneratorId = urlParams.get("new_letter_generator_id");
+  if (newLetterGeneratorId) {
+    const newLetterElement = document.querySelector("[data-generator-id]");
+    newLetterElement.classList.add("new-letter-created");
+  }
 
-    if (newLetterGeneratorId) {
-        const newLetterElement = document.querySelector('[data-generator-id]')
-        newLetterElement.classList.add("new-letter-created");
+  window.onDownloadButtonClick = function (event) {
+    ReadIcon(event);
+  };
+
+  window.onLetterViewButtonClick = function (event) {
+    ReadIcon(event);
+  };
+
+  function ReadIcon(event) {
+    // Find the parent card
+    const card = event.currentTarget.closest(".details-card");
+    if (!card) return;
+    // Find the envelope icon in the card
+    const icon = card.querySelector('i[name="iconLetter"]');
+    if (icon) {
+      icon.className = "fa letter-icon fa-envelope-open-o";
     }
-
+  }
 });
