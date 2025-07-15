7a24770adea9f93d653be301daf3a21f82ec77d7
Nathan Felber <nfelber@compassion.ch>
2025-07-15 12:49:27 +0200

    Clean up user router

--- my_compassion/controllers/my2_user.py
+++ my_compassion/controllers/my2_user.py
@@ -18,16 +18,7 @@ class MyCompassionUserController(http.Controller):
         Renders the dashboard page according to the logged-in user's role (sponsor, donator or volunteer).
         return: An HTTP response containing a rendered template with the dashboard.
         """
-        partner = request.env.user.partner_id
-
-        breadcrumbs = [
-            {'name': 'Dashboard', 'url': '/my2/dashboard/', 'active': True},
-        ]
-
         return request.render(
             'my_compassion.my2_dashboard_page',
-            {
-                'sponsorship_ids': partner.sponsorship_ids,
-                'breadcrumbs': breadcrumbs,
-            }
+            {}
         )
