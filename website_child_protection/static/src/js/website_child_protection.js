ebdc865f28acc5fb47848df249fe0ddb11749898
Emanuel Cino <ecino@compassion.ch>
2025-07-28 09:37:08 +0200

    Restore pre-commit prettier to OCA standard
    - Align to what is used by OCA on branch 14

--- website_child_protection/static/src/js/website_child_protection.js
+++ website_child_protection/static/src/js/website_child_protection.js
@@ -1,25 +1,25 @@
 odoo.define("website_child_protection.form", function (require) {
-  "use strict";
+    "use strict";
 
-  var core = require("web.core");
-  var FormEditorRegistry = require("website_form.form_editor_registry");
+    var core = require("web.core");
+    var FormEditorRegistry = require("website_form.form_editor_registry");
 
-  var _t = core._t;
+    var _t = core._t;
 
-  FormEditorRegistry.add("child_protection", {
-    formFields: [
-      {
-        type: "boolean",
-        modelRequired: true,
-        name: "agreed",
-        string: _t("Check to agree to this charter"),
-      },
-      {
-        type: "hidden",
-        modelRequired: true,
-        name: "partner_uuid",
-        string: "UUID",
-      },
-    ],
-  });
+    FormEditorRegistry.add("child_protection", {
+        formFields: [
+            {
+                type: "boolean",
+                modelRequired: true,
+                name: "agreed",
+                string: _t("Check to agree to this charter"),
+            },
+            {
+                type: "hidden",
+                modelRequired: true,
+                name: "partner_uuid",
+                string: "UUID",
+            },
+        ],
+    });
 });
