73d2a147832200495407f2ecb786cbc68497c24d
nico <nhepp@compassion.ch>
2025-07-28 18:03:33 +0200

    [T2370] Personal informations now can be changed. Started working on the other tabs
    Signed-off-by: nico <nhepp@compassion.ch>

--- my_compassion/controllers/my2_user_settings.py
+++ my_compassion/controllers/my2_user_settings.py
@@ -9,6 +9,7 @@
 from odoo.http import request
 from odoo import http
 
+
 class MyCompassionUserController(http.Controller):
 
     @http.route('/my2/user_settings', type="http", auth="user", website=True, sitemap=False)
@@ -16,46 +17,36 @@ class MyCompassionUserController(http.Controller):
         partner = request.env.user.partner_id
         currently_editing_info = request.params.get('currently_editing_info') == 'true'
         submitted_info_edited = request.params.get('submitted_info_edited') == 'true'
+        titles = request.env['res.partner.title'].sudo().search([('is_published', '=', True)])
 
-        # TODO: check if there is alredady an old script for this
-        def is_valid_phone(phone):
-            return False
-        def is_valid_email(email):
-            return False
-
-        # Define accepted fields structure
-        profile_edits_accepted_fields = {
-            'title': False,
-            'surname': False,
-            'name': False,
-            'city': False,
-            'zip': False,
-            'phone': False,
-            'email': False,
+        field_map = {
+            'title_change':     ('title', int),
+            'surname_change':   ('lastname', str),
+            'name_change':      ('firstname', str),
+            'city_change':      ('city', str),
+            'address_change':   ('street', str),
+            'zip_change':       ('zip', str),
+            'phone_change':     ('phone', str),
+            'email_change':     ('email', str),
         }
 
-        # Check if any field has a value (user attempted to submit something)
-        if submitted_info_edited:
-            # Go field by field, check if any value is submitted and valid
-            title = request.params.get('title_change')
-            if title and title != '':
-                profile_edits_accepted_fields['title'] = True
-            if request.params.get('surname_change', '').strip():
-                profile_edits_accepted_fields['surname'] = True
-            if request.params.get('name_change', '').strip():
-                profile_edits_accepted_fields['name'] = True
-            if request.params.get('zip_change', '').strip():
-                profile_edits_accepted_fields['zip'] = True
-            if request.params.get('city_change', '').strip():
-                profile_edits_accepted_fields['city'] = True
-            phone = request.params.get('phone_change', '').strip()
-            if phone and is_valid_phone(phone):
-                profile_edits_accepted_fields['phone'] = True
-            email = request.params.get('email_change', '').strip()
-            if email and is_valid_email(email):
-                profile_edits_accepted_fields['email'] = True
+        profile_edits_accepted_fields = {key.split('_')[0]: False for key in field_map}
+        update_values = {}
 
-            # If all fields are accepted, set the variable to False
+        if submitted_info_edited:
+            for param, (field_name, cast_type) in field_map.items():
+                raw_value = request.params.get(param, '').strip()
+                if raw_value:
+                    try:
+                        update_values[field_name] = cast_type(raw_value)
+                        profile_edits_accepted_fields[param.split('_')[0]] = True
+                    except (ValueError, TypeError):
+                        pass
+
+            if all(profile_edits_accepted_fields.values()):
+                partner.write(update_values)
+                partner = request.env['res.partner'].browse(partner.id)
+
             currently_editing_info = not all(profile_edits_accepted_fields.values())
 
         return request.render(
@@ -65,5 +56,6 @@ class MyCompassionUserController(http.Controller):
                 'currently_editing_info': currently_editing_info,
                 'submitted_info_edited': submitted_info_edited,
                 'profile_edits_accepted_fields': profile_edits_accepted_fields,
+                'titles': titles,
             }
         )
