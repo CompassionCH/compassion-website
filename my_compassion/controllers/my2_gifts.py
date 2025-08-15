71832f988270614fc6bb83ee230af5446f4263ce
EliasKeller <elias.keller@gmail.com>
2025-08-15 11:11:39 +0200

    [T2541] REFACTOR:  made whole UI more dynamic

--- my_compassion/controllers/my2_gifts.py
+++ my_compassion/controllers/my2_gifts.py
@@ -8,6 +8,7 @@
 ##############################################################################
 from odoo import http
 from odoo.http import request
+from urllib.parse import urljoin
 
 
 class MyCompassionFundController(http.Controller):
@@ -22,6 +23,28 @@ class MyCompassionFundController(http.Controller):
         if type not in ('fund', 'gift'):
             type = 'fund'
 
+        base_url = request.httprequest.url_root
+
+        page_content = {}
+        if type == 'gift':
+            page_content = {
+                'title': "Gift to your sponsored children",
+                'description': "Send a gift to your sponsored child and Compassion staff from their local  church will support them in purchasing something they really need.",
+                'banner_title': "Gift of compassion",
+                'banner_text': "Every child deserves a secure home, safe water and medicine to keep them healthy.  That’s why when poverty places a child in critical need, we take action.",
+                'banner_btn_text': "Send a gift",
+                'banner_btn_link': urljoin(base_url, "/my2/gifts/?type=fund"),
+            }
+        else:
+            page_content = {
+                'title': "Gift of compassion",
+                'description': "Make a difference and bring hope to children living in extreme poverty. ",
+                'banner_title': "Give to your sponsored children",
+                'banner_text': "Send a gift to your sponsored child. Compassion staff from their local church will support them in purchasing something they really need. ",
+                'banner_btn_text': "Send a gift",
+                'banner_btn_link': urljoin(base_url, "/my2/gifts/?type=gift"),
+            }
+
         domain = [
             ('activate_for_my_compassion', '=', True),
             ('my_compassion_donation_type', '=', type),
@@ -32,5 +55,5 @@ class MyCompassionFundController(http.Controller):
             "my_compassion.my2_fund_page",
             {
                 'my_compassion_gifts': my_compassion_gifts,
-                'type': type,
+                'page_content': page_content,
             })
