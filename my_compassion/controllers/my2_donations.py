91dd0758e72e42ddeb13f6010c593e6b5dda885b
Daniel Palumbo <danpa32@hotmail.com>
2025-08-19 13:44:50 +0200

    Add security check of the partner who has done the sale order before rendering the thank you page

--- my_compassion/controllers/my2_donations.py
+++ my_compassion/controllers/my2_donations.py
@@ -213,14 +213,14 @@ class MyCompassionDonationsController(http.Controller):
         sale_order_id = request.session.get('sale_last_order_id')
         if sale_order_id:
             sale_order = request.env['sale.order'].sudo().browse(sale_order_id)
-            return request.render(
-                "my_compassion.my2_gifts_thank_you_page",
-                {
-                    "sale_order": sale_order,
-                },
-            )
-        else:
-            return request.redirect('/my2/dashboard')
+            current_partner = request.env.user.partner_id
+            # Check that the order belongs to the current user
+            if sale_order.partner_id == current_partner:
+                return request.render(
+                    "my_compassion.my2_gifts_thank_you_page",
+                    {"sale_order": sale_order},
+                )
+        return request.redirect('/my2/dashboard')
 
     @staticmethod
     def _extract_donation_order_line_fields(product_template, post):
