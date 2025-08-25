113e5cbd35bea1c3ef35366eda089f9e4f390e4f
Nico812 <118693240+Nico812@users.noreply.github.com>
2025-08-25 16:34:20 +0200

    T2522 implement user settings (#118)
    * [T2370] Just added the new page, nothing interesting. The commit is so that i can switch branch to take some code from daniel
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2370] Just added the new page, nothing interesting. The commit is so that i can switch branch to take some code from daniel
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2370] Started implementing the logic for the option to change the personal informations.
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2370] Personal informations now can be changed. Started working on the other tabs
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2370] Committing before refactoring the communication preferences using a new technique Nathan showed me
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2370] Seems stable, still need to change some buttons style
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2522] Almost finished, everything works except "edit account settings" button and still need to change the style of a tab selector
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2522] Committing before a refactor so in case I can come back
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2522] Almost done, committing to take some code from nathan's branch
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2522] Switching branch
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2522] Good state. Committing before rebase.
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2522] Good state. Committing before rebase.
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2522] Addressed some minor changes
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2522] pre-commit changes
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2522] pre-commit changes
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2522] Fixed all the problems raised in the PR
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * [T2522] Precommit
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * Fixed the account settings tab, still waiting to fix user tax receipt preference
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * Fixed the issues brought up in the PR
    
    Signed-off-by: nico <nhepp@compassion.ch>
    
    * add missing newline at end of file
    
    ---------
    
    Signed-off-by: nico <nhepp@compassion.ch>
    Co-authored-by: nico <nhepp@compassion.ch>
    Co-authored-by: NoeBerdoz <berdoznoe@gmail.com>

--- my_compassion/__manifest__.py
+++ my_compassion/__manifest__.py
@@ -61,6 +61,7 @@
         "templates/pages/my2_dashboard.xml",
         "templates/pages/my2_donation_details.xml",
         "templates/pages/my2_new_letter.xml",
+        "templates/pages/my2_user_settings.xml",
         "templates/pages/my2_login_template.xml",
         "templates/pages/my2_sponsorships.xml",
         "templates/pages/my2_new_sponsorship_wizard.xml",
