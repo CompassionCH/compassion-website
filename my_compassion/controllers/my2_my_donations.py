07f4bbbf3cc1d7fffd65e2a89d526aa333198091
nico <nhepp@compassion.ch>
2025-08-27 11:34:16 +0200

    Precommit
    Signed-off-by: nico <nhepp@compassion.ch>

--- my_compassion/controllers/my2_my_donations.py
+++ my_compassion/controllers/my2_my_donations.py
@@ -3,9 +3,9 @@ from datetime import datetime, timedelta
 
 from odoo import fields
 from odoo.http import request, route
-from odoo.osv import expression
 
 from odoo.addons.portal.controllers.portal import CustomerPortal
+
 
 class MyDonationController(CustomerPortal):
     def get_paid_invoices_filter(self, partner):
@@ -18,22 +18,32 @@ class MyDonationController(CustomerPortal):
         return paid_invoices_filter
 
     def get_paid_invoices_subset(self, partner, offset, amount):
-        paid_invoices_subset = request.env["account.move"].sudo().search(
-            self.get_paid_invoices_filter(partner),
-            offset=offset,
-            limit=amount,
+        paid_invoices_subset = (
+            request.env["account.move"]
+            .sudo()
+            .search(
+                self.get_paid_invoices_filter(partner),
+                offset=offset,
+                limit=amount,
+            )
         )
         return paid_invoices_subset
 
     def get_paid_invoices_amount(self, partner):
-        number_of_paid_invoices = request.env["account.move"].sudo().search_count(
-            self.get_paid_invoices_filter(partner)
+        number_of_paid_invoices = (
+            request.env["account.move"]
+            .sudo()
+            .search_count(self.get_paid_invoices_filter(partner))
         )
         return number_of_paid_invoices
 
-    @route(['/my2/my-donations', '/my2/my-donations/page/<int:invoice_page>'],
-           type='http', auth="user", website=True)
-    def my_donations(self,invoice_page=1, invoice_per_page=12, **kw):
+    @route(
+        ["/my2/my-donations", "/my2/my-donations/page/<int:invoice_page>"],
+        type="http",
+        auth="user",
+        website=True,
+    )
+    def my_donations(self, invoice_page=1, invoice_per_page=12, **kw):
         partner = request.env.user.partner_id
 
         # Active sponsorships
@@ -41,15 +51,21 @@ class MyDonationController(CustomerPortal):
 
         # Due invoices
         date_filter_up_bound = datetime.today() + timedelta(days=30)
-        due_invoices = request.env["account.move"].sudo().search([
-            ("partner_id", "=", partner.id),
-            ("payment_state", "=", "not_paid"),
-            ("invoice_category", "=", "sponsorship"),
-            ("move_type", "=", "out_invoice"),
-            ("state", "=", "posted"),
-            ("amount_total", "!=", 0),
-            ("invoice_date", "<", fields.Date.to_string(date_filter_up_bound)),
-        ])
+        due_invoices = (
+            request.env["account.move"]
+            .sudo()
+            .search(
+                [
+                    ("partner_id", "=", partner.id),
+                    ("payment_state", "=", "not_paid"),
+                    ("invoice_category", "=", "sponsorship"),
+                    ("move_type", "=", "out_invoice"),
+                    ("state", "=", "posted"),
+                    ("amount_total", "!=", 0),
+                    ("invoice_date", "<", fields.Date.to_string(date_filter_up_bound)),
+                ]
+            )
+        )
 
         # Computing the total price of the active sponsorships grouped per sponsorship frequency and payment method.
         # group_id groups the invoices that have the same payment method and frequency.
@@ -59,15 +75,15 @@ class MyDonationController(CustomerPortal):
             group_price = group.total_amount
             group_frequency = group.month_interval
             # Here I'm assuming that every entry in the group has the same payment method
-            group_currency =  (active_sponsorships.mapped("pricelist_id.currency_id")[:1]).name or ""
+            group_currency = (
+                (active_sponsorships.mapped("pricelist_id.currency_id")[:1]).name or ""
+            )
             tot_cost_per_frequency[group_frequency] = (group_price, group_currency)
 
         # redundant
         paid_invoices_offset = (invoice_page - 1) * invoice_per_page
         paid_invoices_subset = self.get_paid_invoices_subset(
-            partner,
-            paid_invoices_offset,
-            invoice_per_page
+            partner, paid_invoices_offset, invoice_per_page
         )
         total_paid_invoices = self.get_paid_invoices_amount(partner)
         total_pages = math.ceil(total_paid_invoices / invoice_per_page)
@@ -85,20 +101,26 @@ class MyDonationController(CustomerPortal):
         )
         return request.render("my_compassion.my2_my_donations_page", values)
 
-    @route('/my2/my-donations/history', type='json', auth="user", methods=['POST'],
-           website=True)
+    @route(
+        "/my2/my-donations/history",
+        type="json",
+        auth="user",
+        methods=["POST"],
+        website=True,
+    )
     def my_donations_history(self, invoice_page=1, invoice_per_page=12, **kw):
         partner = request.env.user.partner_id
 
         # Paid invoices
         paid_invoices_offset = (int(invoice_page) - 1) * invoice_per_page
-        paid_invoices_subset = self.get_paid_invoices_subset(partner,
-                                                             paid_invoices_offset,
-                                                             invoice_per_page)
+        paid_invoices_subset = self.get_paid_invoices_subset(
+            partner, paid_invoices_offset, invoice_per_page
+        )
 
         # Paid invoices paging
         total_pages = math.ceil(
-            self.get_paid_invoices_amount(partner) / invoice_per_page)
+            self.get_paid_invoices_amount(partner) / invoice_per_page
+        )
 
         history_data = {
             "current_page": int(invoice_page),
@@ -106,9 +128,9 @@ class MyDonationController(CustomerPortal):
             "total_pages": total_pages,
         }
 
-        html = request.env['ir.qweb']._render(
-            'my_compassion.my2_donations_history_content',
+        html = request.env["ir.qweb"]._render(
+            "my_compassion.my2_donations_history_content",
             values=history_data,
         )
 
-        return {'html': html}
+        return {"html": html}
--- my_compassion/static/src/js/my2_my_donations.js
+++ my_compassion/static/src/js/my2_my_donations.js
@@ -1,36 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    odoo.define('my_compassion.donations_pager_simple', function (require) {
-        'use strict';
+    odoo.define("my_compassion.donations_pager_simple", function (require) {
+        "use strict";
 
-        var rpc = require('web.rpc');
+        var rpc = require("web.rpc");
 
         function updateHistory(page) {
-            const historyContainer = document.getElementById('donation_history_container');
+            const historyContainer = document.getElementById("donation_history_container");
 
             if (!historyContainer) {
-                console.error('Donation history container not found.');
+                console.error("Donation history container not found.");
                 return;
             }
 
             rpc.query({
-                route: '/my2/my-donations/history',
+                route: "/my2/my-donations/history",
                 params: {
                     invoice_page: page,
                 },
             }).then(function (result) {
-
                 if (result.html) {
                     historyContainer.outerHTML = result.html;
                 }
 
-                const newUrl = '/my2/my-donations/page/' + page;
-                window.history.pushState({path: newUrl}, '', newUrl);
+                const newUrl = "/my2/my-donations/page/" + page;
+                window.history.pushState({ path: newUrl }, "", newUrl);
             });
         }
 
-        document.addEventListener('click', function (event) {
-            const prevBtn = event.target.closest('#history_pager_prev');
-            const nextBtn = event.target.closest('#history_pager_next');
+        document.addEventListener("click", function (event) {
+            const prevBtn = event.target.closest("#history_pager_prev");
+            const nextBtn = event.target.closest("#history_pager_next");
 
             if (prevBtn) {
                 event.preventDefault();
