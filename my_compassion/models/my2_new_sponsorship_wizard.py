47d256035d2aa2a2fc20d2983087d40093131f56
Nathan Felber <nfelber@compassion.ch>
2025-08-28 11:24:26 +0200

    T2602 Handle authenticated users in forms
    - Move steps config to new sponsorship wizard model
    - Swap communication and payment steps
    - Run pre-commit formatting

--- my_compassion/models/my2_new_sponsorship_wizard.py
+++ my_compassion/models/my2_new_sponsorship_wizard.py
@@ -1,14 +1,45 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class NewSponsorshipWizard(models.TransientModel):
     _name = "new.sponsorship.wizard"
     _description = "New Sponsorship Wizard"
 
-    n_steps = 3
-    step = fields.Integer()
+    STEP_CONFIGS = {
+        "standard": {
+            "public": [
+                "my_compassion.new_sponsorship_wizard_step_user_details",
+                "my_compassion.new_sponsorship_wizard_step_payment_methods",
+            ],
+            "logged_in": [
+                "my_compassion.new_sponsorship_wizard_step_payment_methods",
+            ],
+        }
+    }
 
-    child = fields.Many2one("compassion.child")
+    current_step_idx = fields.Integer()
+    current_step = fields.Many2one(
+        "new.sponsorship.wizard.step",
+        compute="_compute_current_step",
+        readonly=True,
+    )
+    n_steps = fields.Integer(
+        compute="_compute_n_steps",
+        readonly=True,
+    )
+    is_done = fields.Boolean(
+        compute="_compute_is_done",
+        readonly=True,
+    )
+
+    user_id = fields.Many2one(
+        "res.users",
+        required=True,
+    )
+    child_id = fields.Many2one(
+        "compassion.child",
+        required=True,
+    )
 
     # User details fields
     title = fields.Many2one("res.partner.title")
@@ -43,45 +74,99 @@ class NewSponsorshipWizard(models.TransientModel):
     )
     volunteering = fields.Boolean()
 
-    def action_next_step(self):
-        self.ensure_one()
-        if self.step < (self.n_steps - 1):
-            self.step += 1
+    @api.depends("current_step_idx", "user_id")
+    def _compute_current_step(self):
+        for wizard in self:
+            steps = wizard._get_steps()
+            if 0 <= wizard.current_step_idx < len(steps):
+                wizard.current_step = steps[wizard.current_step_idx]
+            else:
+                wizard.current_step = False
 
-    def action_previous_step(self):
-        self.ensure_one()
-        if self.step > 0:
-            self.step -= 1
+    @api.depends("user_id")
+    def _compute_n_steps(self):
+        for wizard in self:
+            wizard.n_steps = len(wizard._get_steps())
 
-    def action_finish_sponsorship(self):
+    @api.depends("current_step_idx", "n_steps")
+    def _compute_is_done(self):
+        for wizard in self:
+            wizard.is_done = wizard.current_step_idx >= wizard.n_steps
+
+    def update(self, post):
+        values = {}
+
+        def update_field(field, key, convert=lambda x: x):
+            try:
+                values[field] = convert(post[key])
+            except (ValueError, TypeError, KeyError):
+                pass
+
+        update_field("title", "title", int)
+        update_field("lastname", "lastname")
+        update_field("firstname", "firstname")
+        update_field("birthdate", "birthdate")
+        update_field("email", "email")
+        update_field("phone", "phone")
+        update_field("street", "street")
+        update_field("street_number", "street_number")
+        update_field("zip", "zip")
+        update_field("city", "city")
+        update_field("country", "country")
+
+        update_field("payment_method", "payment_method", int)
+        update_field("sponsorship_plus", "sponsorship_plus", bool)
+
+        spoken_languages_ids = [
+            int(post.get(key)) for key in post if key.startswith("spoken_language")
+        ]
+        if spoken_languages_ids:
+            values["spoken_languages"] = [(6, 0, spoken_languages_ids)]
+        update_field("lead_source", "lead_source", int)
+        update_field("volunteering", "volunteering", bool)
+
+        self.write(values)
+
+        # Move to previous / next step
+        action = post.get("action")
+        if action == "next":
+            if self.current_step_idx <= self.n_steps:
+                self.current_step_idx += 1
+        elif action == "previous":
+            if self.current_step_idx > 0:
+                self.current_step_idx -= 1
+
+    def finish_sponsorship(self):
         self.ensure_one()
 
-        # Look for existing partner, create one if not found
-        partner_vals = {
-            "title": self.title.id,
-            "lastname": self.lastname,
-            "firstname": self.firstname,
-            "birthdate_date": self.birthdate,
-            "email": self.email,
-            "phone": self.phone,
-            "street": f"{self.street} {self.street_number}",
-            "zip": self.zip,
-            "city": self.city,
-            "country_id": self.country.id,
-            "spoken_lang_ids": [(4, lang.id) for lang in self.spoken_languages],
-            "interested_for_volunteering": self.volunteering,
-        }
-        partner = self.env["res.partner.match"].match_values_to_partner(
-            partner_vals, match_update=False, match_create=False
-        )
-        if not partner:
-            partner = self.env["res.partner"].create(partner_vals)
+        partner = self.user_id.partner_id
+        if self.user_id._is_public():
+            # Look for existing partner, create one if not found
+            partner_vals = {
+                "title": self.title.id,
+                "lastname": self.lastname,
+                "firstname": self.firstname,
+                "birthdate_date": self.birthdate,
+                "email": self.email,
+                "phone": self.phone,
+                "street": f"{self.street} {self.street_number}",
+                "zip": self.zip,
+                "city": self.city,
+                "country_id": self.country.id,
+                "spoken_lang_ids": [(4, lang.id) for lang in self.spoken_languages],
+                "interested_for_volunteering": self.volunteering,
+            }
+            partner = self.env["res.partner.match"].match_values_to_partner(
+                partner_vals, match_update=False, match_create=False
+            )
+            if not partner:
+                partner = self.env["res.partner"].create(partner_vals)
 
         # Create new sponsorship
         sponsorship = self.env["recurring.contract"].create(
             {
                 "partner_id": partner.id,
-                "child_id": self.child.id,
+                "child_id": self.child_id.id,
                 "payment_mode_id": self.payment_method.id,
                 "type": "S",
             }
@@ -97,3 +182,25 @@ class NewSponsorshipWizard(models.TransientModel):
 
         # Return the new sponsorship
         return sponsorship
+
+    def _get_steps(self):
+        xml_ids = self.STEP_CONFIGS["standard"][
+            "public" if self.user_id._is_public() else "logged_in"
+        ]
+        return [self.env.ref(xml_id).id for xml_id in xml_ids]
+
+
+class NewSponsorshipWizardStep(models.Model):
+    _name = "new.sponsorship.wizard.step"
+    _description = "New Sponsorship Wizard Step"
+
+    title = fields.Char()
+
+    template = fields.Many2one(
+        "ir.ui.view",
+        string="Qweb Template",
+        required=True,
+        domain=[
+            ("type", "=", "qweb"),
+        ],
+    )
