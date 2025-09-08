bf5a0e361e382e1ea9d1ed69a42c15ce0aba148a
Nathan Felber <nathan@pascaline.org>
2025-09-08 14:06:07 +0200

    Add custom amount limitation on donation (frontend) (#146)
    * Add custom amount limitation on donation (frontend)
    
    * Address Gemini comments
    
    ---------
    
    Co-authored-by: Nathan Felber <nfelber@compassion.ch>

--- my_compassion/__manifest__.py
+++ my_compassion/__manifest__.py
@@ -77,6 +77,7 @@
         "templates/components/my2_donation_item.xml",
         "templates/components/my2_donation_product.xml",
         "templates/components/my2_donation_form.xml",
+        "templates/components/my2_giving_limits_table.xml",
         "templates/components/my2_giving_limits_modal.xml",
         "templates/components/my2_checkout.xml",
     ],
