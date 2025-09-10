3f13e4748133dcf6ac840a6206c34bfd55100f08
NoeBerdoz <berdoznoe@gmail.com>
2025-09-10 13:27:52 +0200

    Add info logs on install hook

--- theme_compassion_2025/hooks.py
+++ theme_compassion_2025/hooks.py
@@ -2,11 +2,17 @@ def _post_init_hook(cr, registry):
     """
     This hook is called after the module is installed.
     """
+    import logging
     from odoo import SUPERUSER_ID, api
 
+    _logger = logging.getLogger(__name__)
+
     env = api.Environment(cr, SUPERUSER_ID, {})
+    _logger.info("Generating theme stylesheets via post-init hook...")
 
     # Generate the stylesheets for each model
     env["theme.compassion.colors"]._generate_stylesheet()
     env["theme.compassion.icons"]._generate_stylesheet()
     env["theme.compassion.pictograms"]._generate_stylesheet()
+
+    _logger.info("Theme stylesheet generation complete.")
