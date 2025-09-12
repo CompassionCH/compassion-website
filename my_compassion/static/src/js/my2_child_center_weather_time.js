21582fef4de915fc0c21ffce25069fa692db3ecf
Shayan <119674649+Shayan105@users.noreply.github.com>
2025-09-12 13:54:07 +0200

    Big commit that works

---
+++ my_compassion/static/src/js/my2_child_center_weather_time.js
@@ -0,0 +1,91 @@
+/**
+ * Time updater and Weather Fetcher for Child Profile Center
+ * --------------------------------
+ * This script keeps the current time of the child compassion center updated every minute and fetches the weather infos.
+ * Key Features:
+ * - Fetches weather data in via POST to `/my2/children/<childId>/center-weather`
+ * - Use the injected time zone to display the current time in the center's locale
+ * - Updates the time every 15 seconds in the client side
+ * - Load the weather icon based on the fetched weather data
+ * - Displays the container only after successful data fetch
+ * Used in /templates/pages/my2_child_timeline.xml
+ */
+
+// Mapping of weather icon filenames to their corresponding CSS classes
+const iconClassMap = {
+    "Sun.svg": "weather-icon-sun",
+    "MoonStar.svg": "weather-icon-moon",
+    "CloudRaining04.svg": "weather-icon-rain",
+    "CloudLightning.svg": "weather-icon-rain",
+    "Waves.svg": "weather-icon-mist",
+    "Wind03.svg": "weather-icon-mist",
+    "CloudBlank02.svg": "weather-icon-cloudy",
+};
+document.addEventListener("DOMContentLoaded", () => {
+    const containerEl = document.querySelector(".center-info-card");
+    const timelinePageEl = document.querySelector(".cd-weather-map-container");
+    const currentTimeEl = document.getElementById("current_time");
+    const currentTemperatureEl = document.getElementById("current_temperature");
+    const childId = timelinePageEl.dataset.childId;
+    const timezone = timelinePageEl.dataset.timezone;
+    const weather_icon_el = document.getElementById("weather_icon");
+
+    // Handles time computation and formating
+    if (currentTimeEl) {
+        //Formatting options for the time to be shown
+        const options = {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true,
+            timeZone: timezone,
+        };
+
+        const updateTime = () => {
+            try {
+                const now = new Date();
+                const centerTimeString = now
+                    .toLocaleTimeString("en-US", options)
+                    .replace("AM", "am")
+                    .replace("PM", "pm");
+                currentTimeEl.textContent = centerTimeString;
+            } catch (error) {
+                console.error("Invalid timezone identifier received from server:", timezone, error);
+                clearInterval(clockInterval);
+            }
+        };
+        updateTime();
+        const clockInterval = setInterval(updateTime, 1000 * 15); // Update every 15 seconds
+    }
+    // Fetch and update the current temperature and weather icon
+    if (currentTemperatureEl && weather_icon_el) {
+        fetch(`/my2/children/${childId}/center-weather`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                const temperature = data.current_temperature;
+                currentTemperatureEl.textContent = `${temperature}`;
+                const icon_id = data.weather_icon_id;
+                weather_icon_el.src = `/theme_compassion_2025/static/src/img/icons/${icon_id}`;
+                //Attach the corresponding class for the icon to allow CSS styling
+                const iconClass = iconClassMap[icon_id];
+                if (iconClass) {
+                    weather_icon_el.classList.add(iconClass);
+                }
+
+                //Upon successful fetch and update, ensure the container is visible
+                if (containerEl) {
+                    containerEl.style.display = "block";
+                }
+            })
+            .catch((error) => {
+                console.error("Failed to fetch temperature:", error);
+                currentTemperatureEl.textContent = "--";
+            });
+    } else {
+        console.error("Required HTML element #current_temperature is missing.");
+    }
+});
