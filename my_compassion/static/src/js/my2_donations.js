94ed44e1a65a2f45ec3b69b2c3fe0736af4dece6
EliasKeller <elias.keller@gmail.com>
2025-10-01 17:13:30 +0200

    [T2551] FIX: disable history navigation during loading new page to prevent exception and improve user experience

--- my_compassion/static/src/js/my2_donations.js
+++ my_compassion/static/src/js/my2_donations.js
@@ -3,14 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
         "use strict";
 
         var rpc = require("web.rpc");
+        let isUpdating = false;
 
         function updateHistory(page) {
+            if (isUpdating) {
+                return;
+            }
+
             const historyContainer = document.getElementById("donation_history_container");
+            const pagerButtons = document.querySelectorAll("#history_pager_prev, #history_pager_next");
+
 
             if (!historyContainer) {
                 console.error("Donation history container not found.");
                 return;
             }
+
+            isUpdating = true;
+            pagerButtons.forEach(btn => btn.classList.add('disabled'));
 
             rpc.query({
                 route: "/my2/donations/history",
@@ -21,6 +31,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (result.html) {
                     historyContainer.outerHTML = result.html;
                 }
+            }).finally(() => {
+                    isUpdating = false;
+                    document.querySelectorAll("#history_pager_prev, #history_pager_next").forEach(btn => {
+                    if(btn) btn.classList.remove('disabled');
+                });
             });
         }
 
