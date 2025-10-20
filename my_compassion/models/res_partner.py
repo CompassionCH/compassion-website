4d177266d6f2608beb1ee1ae94fd7c06f16568c0
EliasKeller <elias.keller@gmail.com>
2025-10-20 08:36:34 +0200

    [T2614] REFACTOR: pre-commit

--- my_compassion/models/res_partner.py
+++ my_compassion/models/res_partner.py
@@ -21,19 +21,19 @@ class Partner(models.Model):
     )
 
     ebill_status = fields.Selection(
-        [('subscribed', 'Subscribed'), ('not_subscribed', 'Not Subscribed')],
-        compute='_compute_ebill_status',
-        string='E-Bill Status'
+        [("subscribed", "Subscribed"), ("not_subscribed", "Not Subscribed")],
+        compute="_compute_ebill_status",
+        string="E-Bill Status",
     )
 
     def _compute_ebill_status(self):
         for partner in self:
-        #This assumes you have a model 'ebill.payment.contract'
-            has_active_ebill = self.env['ebill.payment.contract'].search_count([
-                ('partner_id', '=', partner.id),
-                ('state', '=', 'open')
-            ])
-            partner.ebill_status = 'subscribed' if has_active_ebill else 'not_subscribed'
+            has_active_ebill = self.env["ebill.payment.contract"].search_count(
+                [("partner_id", "=", partner.id), ("state", "=", "open")]
+            )
+            partner.ebill_status = (
+                "subscribed" if has_active_ebill else "not_subscribed"
+            )
 
     def _compute_user_login(self):
         for partner in self:
