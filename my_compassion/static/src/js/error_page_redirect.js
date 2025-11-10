9562e5e4b5f02df2fbfb0184cbc126f15eb8e9d0
jordyBSK <dylan.bossoku@jobtrek.ch>
2025-11-10 09:36:03 +0100

    fix: fix files with pre-commit

--- my_compassion/static/src/js/error_page_redirect.js
+++ my_compassion/static/src/js/error_page_redirect.js
@@ -1,28 +1,27 @@
-odoo.define('my_compassion.error_page_redirect', function (require) {
-    'use strict';
+odoo.define("my_compassion.error_page_redirect", function (require) {
+    "use strict";
 
-    const publicWidget = require('web.public.widget');
+    const publicWidget = require("web.public.widget");
 
     publicWidget.registry.RedirectDashboard = publicWidget.Widget.extend({
         selector: "[data-custom='dashboard']",
         events: {
-            click: '_onClickDashboard',
+            click: "_onClickDashboard",
         },
 
         _onClickDashboard() {
-            window.location.href = '/my2/dashboard';
+            window.location.href = "/my2/dashboard";
         },
     });
 
     publicWidget.registry.RedirectContactUs = publicWidget.Widget.extend({
         selector: "[data-custom='contact_us']",
         events: {
-            click: '_onClickContactUs',
+            click: "_onClickContactUs",
         },
 
         _onClickContactUs() {
-            window.location.href = '/contactus';
+            window.location.href = "/contactus";
         },
     });
-
 });
