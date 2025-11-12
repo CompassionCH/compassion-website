6b25af73aede85cc5f1d23647baf0d64ee4e9c3b
EliasKeller <elias.keller@gmail.com>
2025-11-12 11:50:19 +0100

    [T2679] REFACTOR: readded wrongly removed js comment documentation (formatting)

--- my_compassion/static/src/js/my2_new_sponsorship_wizard.js
+++ my_compassion/static/src/js/my2_new_sponsorship_wizard.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
             },
 
             /**
-            * Handles the click on "Next" or "Previous" buttons.
+             * Handles the click on "Next" or "Previous" buttons.
              * @param {Event} ev
              */
             _onStepClick: function (ev) {
