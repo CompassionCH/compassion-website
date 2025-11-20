8834016b07d0c7bacb487e86217ed47789484a0d
EliasKeller <elias.keller@gmail.com>
2025-11-20 14:54:29 +0100

    [T2468] FEAT: init test structure

---
+++ my_compassion/static/src/js/tours/donation_tour.js
@@ -0,0 +1,65 @@
+odoo.define('compassion_website.donation_tour', function (require) {
+      "use strict";
+
+    var tour = require('web_tour.tour');
+
+    tour.register('donation_tour_full_cycle', {
+        test: true,
+        url: '/my2/gift-package/add'
+    }, [
+        {
+            content: "Search for the test product and click on Learn More",
+            trigger: ".donation-product-container:contains('Test Donation Goat') a:contains('Learn More')",
+            run: "click"
+        },
+        {
+            content: "Check if we are on the details page (H2 header)",
+            trigger: ".donation-details-header h2:contains('Test Donation Goat')",
+            run: "text"
+        },
+        {
+            content: "Click on 'Monthly'",
+            trigger: ".my2_donation_form .donation-frequency label:contains('Monthly')",
+            run: "click"
+        },
+        {
+            content: "Select 'Medium' amount",
+            trigger: ".my2_donation_form label[for='donation-suggested-medium']",
+            run: "click"
+        },
+        {
+            content: "Select a sponsored child",
+            trigger: ".my2_donation_form select[name='recipient']",
+            run: "text Calvin"
+        },
+        {
+            content: "Click on Add & check out",
+            trigger: ".my2_donation_form button:contains('Add & check out')",
+            run: "click"
+        },
+        {
+            content: "Check if we are in the cart (Gift Package)",
+            trigger: "h1:contains('Gift Package'), .my2_gift_package_page",
+            run: "text"
+        },
+        {
+            content: "Check if the product is in the list",
+            trigger: "body:has(div:contains('Test Donation Goat')), body:has(td:contains('Test Donation Goat'))",
+            run: function () {
+                console.log("Product successfully found in cart!");
+            }
+        },
+        {
+            content: "Click on the delete button",
+            trigger: "i.icon-trash01, .action-button i[class*='trash']",
+            run: "click"
+        },
+        {
+            content: "Wait until the item is gone",
+            trigger: "body:not(:has(:contains('Test Donation Goat')))",
+            run: function () {
+                console.log("Product successfully deleted!");
+            }
+        }
+    ]);
+});
