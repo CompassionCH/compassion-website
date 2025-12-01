5aa690356236ef44f4ad16cd57c4fea21380fb46
Emanuel Cino <ecino@compassion.ch>
2025-12-01 08:22:35 +0100

    Christmas texts 2026

--- my_compassion/controllers/auto_texts.py
+++ my_compassion/controllers/auto_texts.py
@@ -3,27 +3,31 @@ CHRISTMAS_TEXTS = {
     "fra": """Bonjour %s,
 
 
-Je te souhaite de belles fêtes de Noël!
+Bonne nouvelle de Bethléem !
 
-Il y a très longtemps, par une nuit silencieuse, des bergers gardaient leurs moutons. Soudain, un ange leur apparut et leur apporta une merveilleuse nouvelle: Jésus était né! Les bergers furent d'abord effrayés, mais l'ange leur dit de se réjouir, car l'enfant apportait la paix et l'espoir à tous les hommes.
+Très loin d'ici, à Bethléem, un petit bébé du nom de Jésus est né, entouré d'amour, dans une petite étable. Marie et Joseph étaient allés à Bethléem et n'avaient pas trouvé de meilleur endroit où passer la nuit. C'est ainsi que Jésus est venu au monde - dans une simple crèche.
 
-Les bergers se mirent alors immédiatement en route pour voir l'enfant. Lorsqu'ils le trouvèrent, ils surent que Dieu avait préparé un grand cadeau pour nous tous. C'est la joie que nous pouvons célébrer chaque année à Noël.
+Même loin de chez eux, ils ont rapidement reçu de la visite. Des anges avaient annoncé la bonne nouvelle à des bergers, qui sont venus voir le bébé. Jésus a aussi reçu la visite de trois rois mages, qui ont fait tout un voyage pour lui apporter des cadeaux !
 
-Je te souhaite le meilleur pour la nouvelle année, à toi et à ceux que tu aimes.
+Jésus est venu donner la joie, la paix et l’espérance au monde entier.
+
+Je souhaite que cette lumière remplisse aussi ton cœur et que tu passes un très joyeux Noël !
 
 
 %s
 """,
-    "eng": """Hello %s,
+    "eng": """Hey %s,
 
 
-I wish you a very merry Christmas!
+Good news from Bethlehem!
 
-A long time ago, on a quiet night, shepherds were watching over their sheep. Suddenly, an angel appeared to them and brought wonderful news: Jesus was born! The shepherds were scared at first, but the angel told them to rejoice because the child would bring peace and hope to all people.
+Far away, in Bethlehem, a little baby named Jesus was born, surrounded by love, in a little stable. Mary and Joseph had gone to Bethlehem and couldn’t find a better place to spend the night. That’s how Jesus came into the world - in a manger.
 
-The shepherds immediately set off to see the child, and when they found him, they knew that God had prepared a great gift for us all. This is the joy that we celebrate every year at Christmas.
+Even far from home, it wasn’t long before they had visitors. Angels had announced the good news to shepherds, who came to see the baby. Jesus was even visited by three wise kings, who had gone on a long trip to bring him gifts!
 
-I wish you and your loved ones all the best for the New Year.
+Jesus came to give joy, peace, and hope to the whole world.
+
+I hope this light fills your heart too, and that you have a very Merry Christmas!
 
 
 %s
@@ -31,13 +35,14 @@ I wish you and your loved ones all the best for the New Year.
     "por": """Olá %s,
 
 
-Desejo a você um Natal maravilhoso!
+Boas notícias de Belém!
+Muito longe daqui, em Belém, nasceu um pequeno bebê chamado Jesus, rodeado de amor, em um pequeno estábulo. Maria e José tinham ido a Belém e não encontraram melhor lugar para passar a noite. Foi assim que Jesus veio ao mundo - em uma simples manjedoura.
 
-Há muito tempo, em uma noite tranquila, os pastores estavam cuidando de suas ovelhas. De repente, um anjo apareceu para eles e trouxe uma notícia maravilhosa: Jesus havia nascido! A princípio, os pastores ficaram assustados, mas o anjo lhes disse para se alegrarem porque a criança traria paz e esperança a todas as pessoas.
+Mesmo longe de casa, eles rapidamente receberam visitas. Anjos anunciaram a boa notícia aos pastores, que vieram ver o bebê. Jesus também recebeu a visita de três reis magos, que fizeram uma longa viagem para lhe trazer presentes!
 
-Os pastores partiram imediatamente para ver a criança e, quando a encontraram, souberam que Deus havia preparado um grande presente para todos nós. Esta é a alegria que podemos celebrar todos os anos no Natal.
+Jesus veio trazer alegria, paz e esperança ao mundo inteiro.
 
-Desejo a você e a seus entes queridos tudo de bom para o Ano Novo.
+Desejo que essa luz também encha seu coração e que você tenha um Natal muito feliz!
 
 
 %s
@@ -45,13 +50,13 @@ Desejo a você e a seus entes queridos tudo de bom para o Ano Novo.
     "spa": """Hola %s
 
 
-¡Te deseo una maravillosa Navidad!
+¡Buenas noticias desde Belén!
 
-Hace mucho tiempo, en una noche tranquila, unos pastores cuidaban de sus ovejas. De repente, se les apareció un ángel y les dio una noticia maravillosa: ¡Jesús había nacido! Los pastores se asustaron al principio, pero el ángel les dijo que se alegraran porque el niño traería paz y esperanza a todos los pueblos.
+En un pequeño establo, lejos de aquí, nació Jesús, un pequeño bebé rodeado de amor. María y José habían viajado a Belén y no encontraron un alojamiento mejor. Así fue como Jesús vino al mundo en un sencillo pesebre. Estaban lejos de su hogar y, aun así, pronto recibieron visitas. Los ángeles anunciaron la buena noticia, y los pastores vinieron a ver al bebé. Incluso los tres sabios emprendieron el camino y llevaron regalos a Jesús. Jesús vino para traer alegría, paz y esperanza a todo el mundo.
 
-Los pastores salieron inmediatamente a ver al niño y, cuando lo encontraron, supieron que Dios había preparado un gran regalo para todos nosotros. Esta es la alegría que podemos celebrar cada año en Navidad.
+Te deseo que esta luz ilumine también tu corazón y te regale muchos momentos felices en Navidad.
 
-¡Te deseo a ti y a tus seres queridos todo lo mejor para el Año Nuevo!
+¡Feliz Navidad!
 
 
 %s
