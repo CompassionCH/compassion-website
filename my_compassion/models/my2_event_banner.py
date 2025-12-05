483623fd8a37826c804cb16a34e816af7d786231
EliasKeller <elias.keller@gmail.com>
2025-12-05 15:04:27 +0100

    [T2702] REFACTOR: pre-commit

--- my_compassion/models/my2_event_banner.py
+++ my_compassion/models/my2_event_banner.py
@@ -106,14 +106,14 @@ class EventBanner(models.Model):
         "my2.website.route",
         string="Target Pages",
         help="If pages are selected, the banner is only visible on those pages. "
-             "If empty, it is visible on all pages.",
+        "If empty, it is visible on all pages.",
     )
 
     target_partner_tags = fields.Many2many(
         comodel_name="res.partner.category",
         string="Target Contact Tags",
         help="If tags are selected, the banner is only visible to users having "
-             "at least one of these tags. If empty, it is visible to everyone.",
+        "at least one of these tags. If empty, it is visible to everyone.",
     )
 
     def name_get(self):
