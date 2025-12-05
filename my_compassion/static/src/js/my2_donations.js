a0a4e6202403805264a2186b6c13367ba1f03a8a
SamBachmann <samuel.bachmann02@gmail.com>
2025-12-05 14:46:05 +0100

    [T2534] REFACTOR clean up the js

--- my_compassion/static/src/js/my2_donations.js
+++ my_compassion/static/src/js/my2_donations.js
@@ -4,6 +4,7 @@ odoo.define('my_compassion.my2_donations', function (require) {
     var publicWidget = require('web.public.widget');
     var core = require('web.core');
     var QWeb = core.qweb;
+    const ToastService = require("my_compassion.toast_service");
     var _t = core._t;
 
     publicWidget.registry.My2Donations = publicWidget.Widget.extend({
@@ -17,7 +18,6 @@ odoo.define('my_compassion.my2_donations', function (require) {
         events: {
             'open_payment_method_selector': '_onOpenPaymentSelector',
 
-            // New dedicated handlers
             'open_payment_method_update': '_onOpenPaymentMethodUpdate',
             'open_payment_method_change': '_onOpenPaymentMethodChange',
             'open_payment_method_add': '_onOpenPaymentMethodAdd',
@@ -34,11 +34,17 @@ odoo.define('my_compassion.my2_donations', function (require) {
             console.log("My2Donations Public Widget started.");
             var self = this;
 
-            // Bind Bootstrap modal hidden event
-            // We bind to 'body' because Bootstrap sometimes moves modals to the end of the DOM
-            $('body').on('hidden.bs.modal', '#payment_method_selector_modal_update', function () {
-                self._onModalHidden($(this));
+            // 1. Global Listener for Modal Hidden (Cleanup)
+            $('body').on('hidden.bs.modal', '.modal', function () {
+                // Check if it's one of our payment modals
+                if ($(this).attr('id') && $(this).attr('id').startsWith('payment_method_selector_modal')) {
+                    self._onModalHidden($(this));
+                }
             });
+
+            // 2. Global Listener for Payment Selection Change
+            // Allows handling events even if Bootstrap moves the modal outside our widget's scope
+            $('body').on('change', 'input[name="payment_method_selection"]', this._onMethodSelectionChange.bind(this));
 
             return this._super.apply(this, arguments);
         },
@@ -53,11 +59,15 @@ odoo.define('my_compassion.my2_donations', function (require) {
         // -------------------------------------------------------------------------
 
         _onOpenPaymentMethodUpdate: function (ev) {
-            // var detail = ev.originalEvent ? ev.originalEvent.detail : ev.detail;
-            // var groupId = detail ? detail.group_id : null;
-            // var methodInfo = detail ? detail.method_info : null;
-            // console.log("Opening Payment Method Update Modal for Group ID:", methodInfo);
+            var detail = ev.originalEvent ? ev.originalEvent.detail : ev.detail;
+            var groupId = detail ? detail.group_id : null;
+            var methodInfo = detail ? detail.method_info : null;
+
             var $modalUpdate = $('#payment_method_selector_modal_update');
+            var $container = $modalUpdate.find('#modal_container').empty();
+            $container.html(QWeb.render('my_compassion.PaymentMethodUpdateAccordion', methodInfo));
+
+            this._renderPaymentMethods($modalUpdate, groupId, '#payment_methods_switch_container');
             $modalUpdate.modal('show');
         },
 
@@ -66,159 +76,36 @@ odoo.define('my_compassion.my2_donations', function (require) {
             var groupId = detail ? detail.group_id : null;
             var contractId = detail ? detail.contract_id : null;
             var childName = detail ? detail.child_name : null;
+
             console.log("Opening Payment Method Change Modal for Group ID:", groupId, "Contract ID:", contractId, "Child Name:", childName);
             var $modalChange = $('#payment_method_selector_modal_change');
-            $modal.find('#modal_description').empty().text('Change your payment method for ' + (childName || 'your sponsored child') + '.');
-            
+            $modalChange.find('#modal_description').empty().text('Change your payment method for ' + (childName || 'your sponsored child') + '.');
+            $modalChange.data('contract-id', contractId);
+
+            this._renderPaymentMethods($modalChange, groupId, '#modal_container');
 
             $modalChange.modal('show');
         },
 
-         _onOpenPaymentSelector: function (ev) {
-            var detail = ev.originalEvent ? ev.originalEvent.detail : ev.detail;
-            if (!detail) return;
-            console.log("Opening payment selector with detail:", detail);
+        // Helper to render payment methods into a modal
+        _renderPaymentMethods: function ($modal, groupId, cardContainer) {
+            var paymentMethods = $modal.data('payment-methods');
+            var $modalContainer = $modal.find(cardContainer);
+            $modalContainer.empty();
 
-            var $modalUpdate = $('#payment_method_selector_modal_update');
-            var $modalAdd = $('#payment_method_selector_modal_add');
-
-            var button = $(ev.relatedTarget); // Button that triggered the modal
-            var groupId = button.data('group-id');
-
-            console.log("Modal Add:", $modalAdd);
-            console.log("Modal Update:", $modalUpdate);
-            console.log("Group ID from button:", groupId);
-
-
-                // if (!$modal.length) {
-                //     console.error("Modal #payment_method_selector_modal not found.");
-                //     return;
-                // }
-
-                // // 1. Update Texts
-                // var modalType = detail.modal_type || 'Update';
-                // var title = modalType + ' Payment Method';
-                // $modal.find('#modal_title').text(title);
-
-                // // Define Descriptions based on Type
-                // var descriptions = {
-                //     'Update': 'Update your payment method.',
-                //     'Add': 'Add a new payment method to your account.',
-                //     'Change': 'Change your payment method for ' + (detail.child_name || 'your sponsored child') + '.',
-                // };
-                // // Set description
-                // $modal.find('#modal_description').text(descriptions[detail.modal_type]);
-
-                // // Store Context
-                // $modal.data('modal-type', modalType);
-                // $modal.data('group-id', detail.group_id);
-
-                // // Update hidden input
-                // $('#selected_contract_group_id').val(detail.group_id);
-
-                // // Load Methods Logic
-                // // Clear previous list
-                // $modal.find('#modal_container').empty();
-
-                // if (modalType == 'Change') {
-                //     $modal.data('contract-id', detail.contract_id);
-                //     this._loadPaymentMethods($modal);
-                // }
-
-                // if (modalType == 'Update') {
-                //     var $container = $modal.find('#modal_container');
-
-                //     /*
-                //     var group_info = detail.group_info || {};
-                //     var formHtml = QWeb.render('my_compassion.PaymentMethodUpdateForm', group_info);
-                //     console.log("Rendering Update Form with data:", formHtml);
-                //     $container.html(formHtml);
-
-                //     var $listWrapper = $('<div/>');
-                //     $container.append($listWrapper);
-                //     this._loadPaymentMethods($modal, $listWrapper);
-                //     */
-                //     // --- UPDATE MODE: ACCORDION (Form + Switch List) ---
-
-                //     // Create Wrapper
-                //     var $accordion = $('<div/>', { id: 'payment_method_accordion', class: 'accordion' });
-
-                //     // A. Update Form Section
-                //     var info = detail.method_info || detail.group_info || {};
-                //     info.is_card = (info.type === 'token' || info.is_card);
-
-                //     var $headerForm = this._createAccordionSectionHeader('collapseForm', _t('Update Current Details'), true);
-                //     var $bodyForm = $('<div/>', { id: 'collapseForm', class: 'collapse show', 'data-parent': '#payment_method_accordion' });
-                //     var $contentForm = $('<div/>', { class: 'pt-3 pl-1 pr-1' });
-
-                //     $contentForm.html(QWeb.render('my_compassion.PaymentMethodUpdateForm', info));
-                //     $bodyForm.append($contentForm);
-
-                //     $accordion.append($('<div/>', { class: 'border-bottom mb-2' }).append($headerForm).append($bodyForm));
-
-                //     // B. Switch List Section
-                //     var $headerList = this._createAccordionSectionHeader('collapseList', _t('Switch Payment Method'), false);
-                //     var $bodyList = $('<div/>', { id: 'collapseList', class: 'collapse', 'data-parent': '#payment_method_accordion' });
-                //     var $contentList = $('<div/>', { class: 'pt-3' });
-
-                //     this._loadPaymentMethods($modal, $contentList); // Load into specific container
-                //     $bodyList.append($contentList);
-
-                //     $accordion.append($('<div/>', { class: 'border-bottom' }).append($headerList).append($bodyList));
-
-                //     $container.append($accordion);
-
-                //     // Bind visual events (arrows, button text)
-                //     this._bindAccordionEvents($modal);
-
-                //     // Initial Button Text
-                //     $modal.find('#btn_save_payment_method').text(_t('Update Details'));
-
-
-               // }
-
-            // $modal.modal('show');
-        },
-
-        _loadPaymentMethods: function ($modal, $targetContainer) {
-            var $container = $targetContainer || $modal.find('#modal_container');
-            console.log("Loading payment methods into container:", $container);
-
-            // Render Loading Spinner
-            // Ensure 'my_compassion.PaymentMethodLoading' exists in your XML file
-            $container.html(QWeb.render('my_compassion.PaymentMethodLoading'));
-
-            this._rpc({
-                route: '/my2/donations/get_payment_methods_sponsor',
-                params: {}
-            }).then(function (methods) {
-                $container.empty();
-
-                if (!methods || methods.length === 0) {
-                    $container.html('<div class="text-center text-muted p-3">' + _t('No payment methods found.') + '</div>');
-                    return;
+            _.each(paymentMethods, function (method) {
+                if (method.group_id == groupId) {
+                    method.selected = true;
                 }
-
-                // Render Cards
-                _.each(methods, function (method) {
-                    console.log("Rendering payment method:", method);
-                    if (method.group_id == $modal.data('group-id')) {
-                        method.selected = true;
-                    }
-                    var $card = $(QWeb.render('my_compassion.PaymentMethodCard', method));
-                    $container.append($card);
-                });
-            }).catch(function (err) {
-                $container.html('<div class="text-danger text-center">' + _t('Error loading payment methods.') + '</div>');
-                console.error("RPC Error:", err);
+                var $card = $(QWeb.render('my_compassion.PaymentMethodCard', method));
+                $modalContainer.append($card);
             });
         },
-
 
         // Render selected card highlighted
         _onMethodSelectionChange: function (ev) {
             var $input = $(ev.currentTarget);
-            var $container = this.$('#modal_container');
+            var $container = $input.closest('#modal_container');
 
             $container.find('.payment-method-card')
                 .removeClass('selected border-core-blue bg-light-blue')
@@ -231,17 +118,17 @@ odoo.define('my_compassion.my2_donations', function (require) {
 
         _onSavePaymentMethod: function (ev) {
             ev.preventDefault();
-
-
-            var $modal = $('#payment_method_selector_modal');
-            var modal_type = $modal.data('modal-type');
-
             var $btn = $(ev.currentTarget);
+            var $modal = $btn.closest('.modal');
+            var modalType = $modal.data('modal-type');
+
+            console.log("Saving Payment Method. Type:", modalType);
             $btn.prop('disabled', true).prepend('<i class="fa fa-spinner fa-spin mr-1"/>');
 
-            if (modal_type == 'Change') {
+            if (modalType == 'change') {
                 var $selectedInput = $modal.find('input[name="payment_method_selection"]:checked');
                 var new_group_id = $selectedInput.attr('group-id');
+                console.log("Inside the if: new_group_id", new_group_id);
 
                 this._rpc({
                     route: '/my2/donation/change_method_contract',
@@ -252,9 +139,12 @@ odoo.define('my_compassion.my2_donations', function (require) {
                 }).then(function (result) {
                     if (result.success) {
                         $modal.modal('hide');
-                        window.location.reload();
-                        console.log("Payment method changed successfully.");
-                        ToastService.success(_t("Payment method changed successfully."));
+
+                        ToastService.success(_t("Payment method changed successfully."), _t("Success"));
+
+                        // Optional: longer delay if you want them to read it fully
+                        setTimeout(() => window.location.reload(), 2000);
+
                     } else {
                         ToastService.error(result.error || _t("An error occurred while changing the payment method."));
                     }
@@ -262,10 +152,9 @@ odoo.define('my_compassion.my2_donations', function (require) {
                     $btn.prop('disabled', false).find('.fa-spinner').remove();
                 });
 
-            } else if (modal_type == 'Update') {
+            } else if (modalType == 'update') {
                 if ($modal.find('#collapseList').hasClass('show')) {
                     // USER IS SWITCHING METHOD
-                    // this._saveSwitchMethod($modal, $btn, modalType);
                     var $selectedInput = $modal.find('input[name="payment_method_selection"]:checked');
                     var new_group_id = $selectedInput.attr('group-id');
                     console.log("Switching to Payment Method Group ID:", new_group_id);
@@ -286,6 +175,7 @@ odoo.define('my_compassion.my2_donations', function (require) {
                         }
                     }).finally(function () {
                         $btn.prop('disabled', false).find('.fa-spinner').remove();
+                        ToastService.info("TEST")
                     });
                 } else {
                     // USER IS UPDATING DETAILS (Form)
@@ -301,13 +191,13 @@ odoo.define('my_compassion.my2_donations', function (require) {
         },
 
         _onModalHidden: function ($modal) {
-            $modal.removeData(['group-id', 'contract-id', 'modal-type']);
-            $modal.find('#payment_method_modal_title').text(_t('Payment Method'));
-            $modal.find('#payment_method_modal_desc').text(_t('Select a payment method.'));
-            $modal.find('#modal_container').empty();
+            $modal.removeData(['group-id']);
             $modal.find('input[type="radio"]').prop('checked', false);
         },
 
+        // -------------------------------------------------------------------------
+        // Pagination for Donation History
+        // -------------------------------------------------------------------------
         _onPagerClick: function (ev) {
             ev.preventDefault();
             var $btn = $(ev.currentTarget);
