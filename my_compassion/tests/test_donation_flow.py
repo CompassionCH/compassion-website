ddbed3770e40e538e890e0ea10a54c45f71274be
EliasKeller <elias.keller@gmail.com>
2025-12-09 11:43:07 +0100

    [T2468] REFACTOR: better logging

--- my_compassion/tests/test_donation_flow.py
+++ my_compassion/tests/test_donation_flow.py
@@ -9,6 +9,7 @@ _logger = logging.getLogger(__name__)
 
 @tagged('post_install', '-at_install')
 class TestDonationFlow(HttpCase):
+    TEST_DOMAIN = "http://127.0.0.1:8069"
 
     def setUp(self):
         super(TestDonationFlow, self).setUp()
@@ -24,13 +25,15 @@ class TestDonationFlow(HttpCase):
         - Selects the target website (MyCompassion).
         - Forces the environment context to the target website.
         """
+        _logger.info("SETUP (WEBSITE): Configuring MyCompassion as target website for the environment.")
+
         target_website = self.env['website'].sudo().search([('name', '=', 'MyCompassion')], limit=1)
 
         if not target_website:
             raise AssertionError("FATAL: Target website (MyCompassion) not found!")
 
         # Must be localhost so that authentication cookies are accepted
-        target_website.write({'domain': 'http://127.0.0.1:8069/'})
+        target_website.write({'domain': self.TEST_DOMAIN})
 
         # Force environment context to this website
         self.env = self.env(context={'website_id': target_website.id})
@@ -39,6 +42,8 @@ class TestDonationFlow(HttpCase):
         """
         Patches res.users to bypass credential checks during the test.
         """
+        _logger.info("SETUP (AUTH): Patching res.users to bypass authentication checks.")
+
         RegistryResUsers = type(self.env['res.users'])
         self.auth_patcher = patch.object(RegistryResUsers, '_check_credentials', side_effect=None)
         self.auth_patcher.start()
@@ -49,7 +54,9 @@ class TestDonationFlow(HttpCase):
         Patches the Chrome browser spawn process.
         Stores the original method and replaces it with our custom handler.
         """
-        # Save reference to the real function before patching
+
+        _logger.info("SETUP (BROWSER): Patching Chrome browser spawn process.")
+
         self.original_chrome_spawn = ChromeBrowser._spawn_chrome
 
         self.browser_patcher = patch.object(
@@ -61,14 +68,14 @@ class TestDonationFlow(HttpCase):
         self.browser_patcher.start()
         self.addCleanup(self.browser_patcher.stop)
 
+        _logger.info("Patched Chrome browser spawn method.")
+
     def _custom_chrome_spawn_handler(self, browser_instance, cmd):
         """
         Callback that modifies the Chrome command line arguments.
         :param browser_instance: The instance of ChromeBrowser (internally used by Odoo)
         :param cmd: The list of command line arguments
         """
-        _logger.info("### CHROME PATCH APPLIED ###")
-
         new_args = [
             '--headless=new',
             '--disable-component-extensions-with-background-pages',
@@ -82,9 +89,9 @@ class TestDonationFlow(HttpCase):
         return self.original_chrome_spawn(browser_instance, cmd)
 
     def test_donation_tour(self):
-        _logger.info("START: Donation Tour")
+        _logger.info("START TEST RUN: Donation Tour")
 
-        start_url = "http://127.0.0.1:8069/my2/dashboard"
+        start_url = f"{self.TEST_DOMAIN}/my2/dashboard"
         self.browser_js(
             url_path=start_url,
             code="odoo.__DEBUG__.services['web_tour.tour'].run('donation_tour_full_cycle_2')",
