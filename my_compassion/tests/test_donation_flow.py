6fa65f711a05d6528ba3f6ea817e50df01f6c2c8
EliasKeller <elias.keller@gmail.com>
2025-12-09 11:33:50 +0100

    [T2468] REFACTOR: better structure in setUp of the test

--- my_compassion/tests/test_donation_flow.py
+++ my_compassion/tests/test_donation_flow.py
@@ -1,5 +1,4 @@
 import logging
-import random
 from unittest.mock import patch
 
 from odoo.tests import tagged
@@ -10,73 +9,77 @@ _logger = logging.getLogger(__name__)
 
 @tagged('post_install', '-at_install')
 class TestDonationFlow(HttpCase):
-    @classmethod
-    def setUpClass(cls):
-        # benutzt cls.host intern
-        super(TestDonationFlow, cls).setUpClass()
 
     def setUp(self):
         super(TestDonationFlow, self).setUp()
 
-        # ==========================================================================
-        # 2. WEBSITE SELECTION (STRIKT)
-        # ==========================================================================
-        # Wir suchen die Website, auf der der Test laufen SOLL.
-        # Passe 'MyCompassion' an den exakten Namen deiner Website in Odoo an.
-        target_website = self.env['website'].sudo().search([('name', 'ilike', 'MyCompassion')], limit=1)
+        # Execute setup steps
+        self._setup_website()
+        self._patch_authentication()
+        self._patch_browser()
+
+    def _setup_website(self):
+        """
+        Configures the website environment:
+        - Selects the target website (MyCompassion).
+        - Forces the environment context to the target website.
+        """
+        target_website = self.env['website'].sudo().search([('name', '=', 'MyCompassion')], limit=1)
 
         if not target_website:
-            raise AssertionError(f"FATAL: Target website (MyCompassion) not found!")
+            raise AssertionError("FATAL: Target website (MyCompassion) not found!")
 
-        _logger.info(f"### TESTING ON WEBSITE: {target_website.name} (ID: {target_website.id}) ###")
-
-        # Anderen Websites eine Fake-Domain geben, damit Odoo sie auf 127.0.0.1 ignoriert
-        other_websites = self.env['website'].sudo().search([('id', '!=', target_website.id)])
-        if other_websites:
-            other_websites.write({'domain': 'ignore.localhost.test'})
-
-       # must be localhost so that authentication cookies are accepted
+        # Must be localhost so that authentication cookies are accepted
         target_website.write({'domain': 'http://127.0.0.1:8069/'})
 
-        # Environment auf diese Website zwingen
+        # Force environment context to this website
         self.env = self.env(context={'website_id': target_website.id})
 
-        # ------------------------------------------------------------------
-        # Authentifizierung
-        # ------------------------------------------------------------------
+    def _patch_authentication(self):
+        """
+        Patches res.users to bypass credential checks during the test.
+        """
         RegistryResUsers = type(self.env['res.users'])
         self.auth_patcher = patch.object(RegistryResUsers, '_check_credentials', side_effect=None)
         self.auth_patcher.start()
         self.addCleanup(self.auth_patcher.stop)
 
-        # ------------------------------------------------------------------
-        # FIX 2: Chrome Patch
-        # ------------------------------------------------------------------
-        original_spawn = ChromeBrowser._spawn_chrome
-
-        def patched_spawn(browser_self, cmd):
-            _logger.info("### CHROME PATCH APPLIED ###")
-
-            new_args = [
-                '--headless=new',  # New headless architecture (required for modern Chrome)
-                '--disable-component-extensions-with-background-pages',  # Disable default extensions of browser
-                '--window-size=1920,1080',  # Important for stable viewport
-            ]
-
-            for arg in new_args:
-                if arg not in cmd:
-                    cmd.append(arg)
-
-            return original_spawn(browser_self, cmd)
+    def _patch_browser(self):
+        """
+        Patches the Chrome browser spawn process.
+        Stores the original method and replaces it with our custom handler.
+        """
+        # Save reference to the real function before patching
+        self.original_chrome_spawn = ChromeBrowser._spawn_chrome
 
         self.browser_patcher = patch.object(
             ChromeBrowser,
             '_spawn_chrome',
-            side_effect=patched_spawn,
+            side_effect=self._custom_chrome_spawn_handler,
             autospec=True,
         )
         self.browser_patcher.start()
         self.addCleanup(self.browser_patcher.stop)
+
+    def _custom_chrome_spawn_handler(self, browser_instance, cmd):
+        """
+        Callback that modifies the Chrome command line arguments.
+        :param browser_instance: The instance of ChromeBrowser (internally used by Odoo)
+        :param cmd: The list of command line arguments
+        """
+        _logger.info("### CHROME PATCH APPLIED ###")
+
+        new_args = [
+            '--headless=new',
+            '--disable-component-extensions-with-background-pages',
+            '--window-size=1920,1080',
+        ]
+
+        for arg in new_args:
+            if arg not in cmd:
+                cmd.append(arg)
+
+        return self.original_chrome_spawn(browser_instance, cmd)
 
     def test_donation_tour(self):
         _logger.info("START: Donation Tour")
