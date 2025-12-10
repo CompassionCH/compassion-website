41975af3aacdd74574f665b738f878bb4848db7c
EliasKeller <elias.keller@gmail.com>
2025-12-10 09:02:34 +0100

    [T2468] FEAT: single_one_time_gift_with_custom_amount

--- my_compassion/static/src/js/tours/donation_tour.js
+++ my_compassion/static/src/js/tours/donation_tour.js
@@ -55,6 +55,59 @@ odoo.define('compassion_website.donation_tour', function (require) {
         },
     ]);
 
+    tour.register('single_one_time_gift_with_custom_amount', {
+        test: true,
+        url: '/my2/gifts',
+    }, [
+        {
+            content: "Select the Test Product from the list",
+            trigger: `.card.vignette h3:contains("${MOCK_TEST_DATA.product_name}")`,
+            run: "click"
+        },
+        {
+            content: "Check if we are on the details page (H2 header)",
+            trigger: `.donation-details-header h2:contains("${MOCK_TEST_DATA.product_name}")`,
+            run: "text"
+        },
+        {
+            content: "Click on 'One Time'",
+            trigger: ".my2_donation_form .donation-frequency label:contains('One Time')",
+            run: "click"
+        },
+        {
+            content: "Select 'Custom amount' radio button",
+            trigger: "label[for='donation-suggested-custom']",
+            run: "click"
+        },
+        {
+            content: "Enter the custom amount (75.00)",
+            trigger: "input[name='custom_amount']",
+            run: "text 75.00"
+        },
+        {
+            content: "Click on Add & check out",
+            trigger: ".my2_donation_form button:contains('Add & check out')",
+            run: "click"
+        },
+        {
+            content: "Check if we are in the cart (Gift Package)",
+            trigger: "h1:contains('Gift Package'), .my2_gift_package_page",
+            run: "text"
+        },
+        {
+            content: "Check if the product is in the list",
+            trigger: `body:has(div:contains("${MOCK_TEST_DATA.product_name}")), body:has(td:contains("${MOCK_TEST_DATA.product_name}"))`,
+            run: function () {
+                console.log("Product successfully found in cart!");
+            }
+        },
+        {
+            content: "Check total amount before delete (CHF 75)",
+            trigger: ".bg-light-green:contains('Total amount'):contains(' 75')",
+            run: "text"
+        },
+    ]);
+
      tour.register('single_monthly_gift_with_suggested_amount', {
         test: true,
         url: '/my2/gifts',
