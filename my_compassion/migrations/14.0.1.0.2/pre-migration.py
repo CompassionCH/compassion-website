40af065e03050b5ac4964a5b83166167f66e8481
SlWa99 <walid.slimani99@gmail.com>
2026-01-09 10:26:46 +0100

    [T2859] STYLE: Run pre-commit

--- my_compassion/migrations/14.0.1.0.2/pre-migration.py
+++ my_compassion/migrations/14.0.1.0.2/pre-migration.py
@@ -4,21 +4,25 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     """
-        Pre-migration script for 14.0.1.0.2.
-        Set and modify XML_ID of some product.template.
-            - Health funds product were without xml_id
-                -> 'product_template_fund_hea'
-            - Food aid program produt had an id of type '__export__.'
-                -> 'product_template_fund_fda'
-            - Emergency funds product had an id of type '__export__.'
-                -> 'product_template_fund_dis'
-        """
+    Pre-migration script for 14.0.1.0.2.
+    Set and modify XML_ID of some product.template.
+        - Health funds product were without xml_id
+            -> 'product_template_fund_hea'
+        - Food aid program produt had an id of type '__export__.'
+            -> 'product_template_fund_fda'
+        - Emergency funds product had an id of type '__export__.'
+            -> 'product_template_fund_dis'
+    """
     cr = env.cr
 
     # Standard processing for ID 474 (did not have an XML ID)
     openupgrade.add_xmlid(
-        cr, "my_compassion", "product_template_fund_hea",
-        "product.template", 474, noupdate=False
+        cr,
+        "my_compassion",
+        "product_template_fund_hea",
+        "product.template",
+        474,
+        noupdate=False,
     )
 
     # Handling existing IDs of type “__export__.” (238 and 303)
@@ -31,15 +35,12 @@ def migrate(env, version):
     for res_id, new_name in mapping.items():
         cr.execute(
             "SELECT module, name FROM ir_model_data WHERE model = %s AND res_id = %s",
-            ('product.template', res_id)
+            ("product.template", res_id),
         )
         res = cr.fetchone()
 
         if res:
             old_module, old_name = res
-            openupgrade.rename_xmlids(cr, [
-                (
-                    f"{old_module}.{old_name}",
-                    f"my_compassion.{new_name}"
-                )
-            ])
+            openupgrade.rename_xmlids(
+                cr, [(f"{old_module}.{old_name}", f"my_compassion.{new_name}")]
+            )
