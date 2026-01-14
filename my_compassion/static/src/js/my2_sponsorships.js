815493e8906dd12f8c66ea02552a2a768bdda037
Samuel Bachmann <93264382+SamBachmann@users.noreply.github.com>
2026-01-14 15:31:37 +0100

    T2846 FIX redirect "Choose for me" btn when the user is Write & Pray (#234)
    * [T2846] FIX redirect "Choose for me" btn when the user is Write & Pray
    
    * [T2846] FIX improve readability of the url

--- my_compassion/static/src/js/my2_sponsorships.js
+++ my_compassion/static/src/js/my2_sponsorships.js
@@ -263,8 +263,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
                         function (data) {
                             if (data.child_id) {
                                 // Redirect to new sponsorship page
-                                window.location.href =
-                                    "/my2/new-sponsorship/" + data.child_id + "?type=" + this.sponsorship_type;
+                                window.location.href = `/my2/new-sponsorship/${data.child_id}?sponsorship_type=${this.sponsorship_type}`;
                             }
                             this.$(".btn").prop("disabled", false);
                         }.bind(this)
