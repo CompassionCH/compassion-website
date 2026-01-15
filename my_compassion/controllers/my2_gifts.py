8d5fc627a54481b63f612445763bcf7fd9ff667f
SlWa99 <walid.slimani99@gmail.com>
2026-01-15 13:24:13 +0100

    [T2866] STYLE: Run pre-commit

--- my_compassion/controllers/my2_gifts.py
+++ my_compassion/controllers/my2_gifts.py
@@ -27,8 +27,7 @@ class MyCompassionGiftController(http.Controller):
         ]
 
         my_compassion_gifts = request.env["product.template"].search(
-            domain,
-            order='sequence'
+            domain, order="sequence"
         )
         return request.render(
             "my_compassion.my2_gifts_page",
