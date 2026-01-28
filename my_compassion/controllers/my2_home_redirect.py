46092c77993ffcb343d135248f7c829ce22abb27
NoeBerdoz <berdoznoe@gmail.com>
2026-01-28 14:36:23 +0100

    fix: redirection to odoo's default homepage logic index method

--- my_compassion/controllers/my2_home_redirect.py
+++ my_compassion/controllers/my2_home_redirect.py
@@ -24,9 +24,11 @@ class WebsiteHomeRedirect(Website):
         `my2_login.py` handles the redirection from the login page to the dashboard.
         """
 
-        if request.website == request.env.ref("my_compassion.my2_website"):
+        if request.website == request.env.ref(
+            "my_compassion.my2_website", raise_if_not_found=False
+        ):
             if request.session.uid:
                 return request.redirect("/my2/dashboard")
             else:
                 return request.redirect("/web/login")
-        return super().home(*args, **kw)
+        return super().index(*args, **kw)
