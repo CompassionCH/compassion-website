9ebb71a736020be4d791e13dd707956d9161a272
Emanuel Cino <ecino@compassion.ch>
2026-04-08 15:49:28 +0200

    MIG theme_muskathlon

--- theme_muskathlon/__manifest__.py
+++ theme_muskathlon/__manifest__.py
@@ -6,12 +6,23 @@
     "version": "18.0.1.0.0",
     "author": "Compassion Switzerland",
     "website": "https://github.com/CompassionCH/compassion-website",
-    "depends": ["website"],
+    "depends": ["website_sale"],
     "data": [
-        "views/assets.xml",
         "views/images_content.xml",
+        "data/presets.xml",
     ],
+    "assets": {
+        "web._assets_primary_variables": [
+            "theme_muskathlon/static/src/scss/primary_variables.scss",
+        ],
+        "web.assets_frontend": [
+            "theme_muskathlon/static/src/scss/font.scss",
+        ],
+        "web._assets_frontend_helpers": [
+            ("prepend", "theme_muskathlon/static/src/scss/bootstrap_overidden.scss"),
+        ],
+    },
     "images": ["static/description/muskathlon_screenshot.jpeg"],
     "license": "LGPL-3",
-    "installable": False,
+    "installable": True,
 }
