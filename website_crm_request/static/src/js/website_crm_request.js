bf02d2b62cea65b11bfd0ee43d2a0aac1c4fde6d
prazn <praznicolas@gmail.com>
2026-06-08 10:46:10 +0200

    [T2911] STYLE : pre-commit

--- website_crm_request/static/src/js/website_crm_request.js
+++ website_crm_request/static/src/js/website_crm_request.js
@@ -3,30 +3,31 @@
 import {_t} from "@web/core/l10n/translation";
 import FormEditorRegistry from "@website/js/form_editor_registry";
 
-FormEditorRegistry.add('crm_request', {
-    formFields: [
-        {
-            type: "char",
-            modelRequired: true,
-            name: "name",
-            string: _t("Subject"),
-        },
-        {
-            type: "email",
-            modelRequired: true,
-            name: "email_from",
-            string: _t("Email"),
-        },
-        {
-            type: "tel",
-            modelRequired: false,
-            name: "partner_phone",
-            string: _t("Phone number"),
-        },
-        {
-            type: "text",
-            modelRequired: true,
-            name: "description",
-            string: _t("Message"),
-        }],
+FormEditorRegistry.add("crm_request", {
+  formFields: [
+    {
+      type: "char",
+      modelRequired: true,
+      name: "name",
+      string: _t("Subject"),
+    },
+    {
+      type: "email",
+      modelRequired: true,
+      name: "email_from",
+      string: _t("Email"),
+    },
+    {
+      type: "tel",
+      modelRequired: false,
+      name: "partner_phone",
+      string: _t("Phone number"),
+    },
+    {
+      type: "text",
+      modelRequired: true,
+      name: "description",
+      string: _t("Message"),
+    },
+  ],
 });
