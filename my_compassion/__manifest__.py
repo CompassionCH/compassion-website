0cef4ba864fec45496169b741852d2d1a3053967
NoeBerdoz <berdoznoe@gmail.com>
2026-07-14 15:44:08 +0200

    [T3297] FIX: cache-safe portal child access rule via type-filtered o2m traversal

--- my_compassion/__manifest__.py
+++ my_compassion/__manifest__.py
@@ -30,7 +30,7 @@
 # pylint: disable=C8101
 {
     "name": "MyCompassion - Sponsor portal website",
-    "version": "18.0.1.0.7",
+    "version": "18.0.1.0.8",
     "category": "Website",
     "author": "Compassion CH",
     "license": "AGPL-3",
